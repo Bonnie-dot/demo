7c08a5853afde4e583ee285dc9de739c07c3dcb2
bonnie <277497288@qq.com>
2023-08-17 22:22:53 +0800

    split production and development script

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const Webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-const webpackConfig = require('./webpack.config.js');
+const webpackConfig = require('./ webpack.dev');
 
 const compiler = Webpack(webpackConfig);
 const devServerOptions = { ...webpackConfig.devServer, open: true };
---
+++ webpack.common.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+    entry: './src/Index.tsx',
+    module: {
+        rules: [
+            {
+                // it handled ts and jsx
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset',
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        clean: true,
+        assetModuleFilename: 'images/[hash][ext][query]'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'WorkShop',
+            template: "./public/template.html",
+            favicon: './public/favicon.ico',
+            hash: true
+        }),
+    ],
+}
